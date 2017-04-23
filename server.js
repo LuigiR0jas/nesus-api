e28af8c6fb52f1769fa1c3b47643f60a39feb151
Luigi Rojas <xluigix98@gmail.com>
2017-04-23 15:45:23 -0400

    request-ip

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express'),
       morgan = require('morgan'),
       mongoose = require('mongoose'),
       config = require('./config.js'),
+      externalip = require('externalip'),
+      requestIp = require('request-ip'),
       port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({
@@ -15,25 +17,27 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \
+    res.setHeader('Access-Control-Allow-Headers', 'X-Forwarded-For,X-Requested-With,content-type, \
     Authorization');
     next();
 });
 
 app.use(morgan('dev'));
 
+app.use(function(req, res, next){
+  // const clientIp = requestIp.getClientIp(req);
+  console.log('Somebody hit the middleware...');
+  next();
+})
+
 app.get('/', function(req, res){
-  var ip = req.connection.remoteAddress;
+  const ip = requestIp.getClientIp(req); 
   res.send('Hello! Your IP ('+ ip +') is being tracked now ;)')
   console.log(ip);
 })
 
 var apiRouter = express.Router();
 
-apiRouter.use(function(req, res, next){
-  console.log('Somebody hit the middleware...');
-  next();
-})
 
 apiRouter.get('/', function(req, res){
   res.send('Nice, youre in the api');
